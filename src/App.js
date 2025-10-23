2023fde69f1e9936b76ca04f59cf825fdc44b830
yyrrcc <music_v@naver.com>
2025-10-23 17:51:29 +0900

    초기 설정 ..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState(""); // 사용자가 새로 입력한 메시지
+
+  const handleEnter = () => {};
+  const handleSubmit = () => {};
+
+  // 첫 마운트 때 가져오기
+  useEffect(() => {
+    new WebSocket("ws://localhost:8888/ws/chat");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>스프링부트 + 리액트 웹소켓 채팅</h3>
+      <div className="msg-list">메시지 배열</div>
+
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && handleEnter}
+        className="new-msg"
+      />
+      <button onClick={handleSubmit}>전송</button>
     </div>
   );
 }
